db27212140d10278a133d86b1b4facb9f2d8eecc
antoineboquet <antoineboquet@outlook.com>
2021-05-13 16:02:05 +0200

    Normalize ano teleia

--- src/toTransliteration.ts
+++ src/toTransliteration.ts
@@ -21,6 +21,8 @@ export function toTransliteration (
       str = applyTransliteratedBreathings(str, keyType.GREEK)
       if (options.removeDiacritics) str = removeDiacritics(str)
       str = removeGreekVariants(str)
+      // Normalize `middle dot` (\u00B7) to `greek ano teleia` (\u0387).
+      str = str.replace(/\u00B7/g, '\u0387')
       str = fromGreekToTransliteration(str)
       break
 
@@ -52,7 +54,7 @@ function fromGreekToTransliteration (str: string): string {
   let newStr = ''
 
   for (let i = 0; i < str.length; i++) {
-    let tmp = undefined
+    let tmp: string = undefined
 
     for (const key of mapping) {
       // `Combining Greek Perispomeni` (\u0342) diacritic is greek-only and must
