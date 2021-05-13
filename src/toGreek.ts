b1f1032bbbe06abd2be561d53bad50eb20aff48c
antoineboquet <antoineboquet@outlook.com>
2021-05-13 22:11:06 +0200

    Clean code

--- src/toGreek.ts
+++ src/toGreek.ts
@@ -3,6 +3,7 @@ import { mapping } from './mapping'
 import {
   applyGammaDiphthongs,
   applyGreekVariants,
+  normalizeGreek,
   removeDiacritics
 } from './utils'
 
@@ -21,8 +22,8 @@ export function toGreek (
     case keyType.TRANSLITERATION:
       if (options.removeDiacritics) str = removeDiacritics(str)
       str = fromTransliterationToGreek(str)
-      // Normalize `middle dot` (\u00B7) to `greek ano teleia` (\u0387).
-      str = str.replace(/\u00B7/g, '\u0387')
+      str = applyBreathings(str)
+      str = normalizeGreek(str)
       break
   }
 
@@ -32,84 +33,84 @@ export function toGreek (
   return str
 }
 
-function fromBetaCodeToGreek (str: string): string {
+function applyBreathings (str: string) {
+  const words = str.split(' ')
+
+  words.forEach((el, i) => {
+    if (el.charAt(0) === 'H') {
+      el = el.slice(1)
+      words[i] = el.charAt(0).toUpperCase() + el.slice(1)
+    }
+  })
+
+  str = words.join(' ')
+  str = str.replace(/h/gi, '')
+
+  return str
+}
+
+function fromBetaCodeToGreek (betaCodeStr: string): string {
   let greekStr = ''
 
-  for (let i = 0; i < str.length; i++) {
-    let tmp: string = undefined
+  for (const char of betaCodeStr) {
+    let tmp: string
 
     for (const key of mapping) {
-      if (key.latin === str[i]) {
+      if (key.latin === char) {
         tmp = key.greek
+        break
       }
     }
 
-    greekStr += (tmp !== undefined) ? tmp : str[i]
+    greekStr += tmp ?? char
   }
 
   return greekStr
 }
 
-function fromTransliterationToGreek (str: string): string {
+function fromTransliterationToGreek (transliteratedStr: string): string {
   let greekStr = ''
 
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < transliteratedStr.length; i++) {
     const tmp = { trans: '', greek: '' }
 
-    let  pair = str.slice(i, i + 2)
+    let pair = transliteratedStr.slice(i, (i + 2))
     if (pair.length !== 2) pair = ''
 
-    // Facultative `h` character: ignore if its position is credible
-
-    // A `h` but no consecutive `h`
-    const rule1 = (/h/i.test(str[i]) && !/h/i.test(str[i + 1]))
-    // A `h` may appear behind a `r`
-    const rule2 = (/[r]/i.test(str[i - 1]))
-    // A `h` may appear in first place (of a string or a word) when it precedes a vowel
-    const rule3 = (
-      (str[i - 1] === undefined || str[i - 1] === ' ')
-      && /[aeêioôu]/i.test(str[i + 1])
-    )
-
-    // When the transliterated breathing is uppercase,
-    // the greek word must start with a capital letter.
-    if (rule3 && str[i] === 'H') {
-      str = str.slice(0, (i + 1)) + str[i + 1].toUpperCase() + str.slice(i + 2)
-    }
-
-    //if (rule1 && (rule2 || rule3)) continue
-    if (/h/i.test(str[i])) continue
-
-    for (let j = 0; j < mapping.length; j++) {
+    for (const key of mapping) {
       // `Combining Tilde` (\u0303) diacritic is latin-only and must be converted
       // to the latin diacritical mark `Combining Greek Perispomeni` (\u0342).
-      let decomposedChar = str[i].normalize('NFD').replace(/\u0303/g, '\u0342')
-
+      let decomposedChar = transliteratedStr[i].normalize('NFD').replace(/\u0303/g, '\u0342')
       // Isolate the character with its potential circumflex (as this diacritical
       // mark is used to represent long vowels in transliterated form).
-      let recomposedChar = decomposedChar.charAt(0)
+      let recomposedChar = recomposeChar(decomposedChar)
 
-      // \u0302 = circumflex.
-      if (decomposedChar.includes('\u0302')) {
-        recomposedChar += '\u0302'
-        decomposedChar = decomposedChar.replace(/\u0302/g, '')
-      }
+      decomposedChar = decomposedChar.replace(/\u0302/g, '') // \u0302 = circumflex.
 
-      recomposedChar = recomposedChar.normalize('NFC')
-
-      if (mapping[j].trans === recomposedChar || mapping[j].trans === pair) {
-        tmp.trans = mapping[j].trans
+      if ([recomposedChar, pair].includes(key.trans)) {
+        tmp.trans = key.trans
         // Char + potential remaining diacritics.
-        tmp.greek = mapping[j].greek + decomposedChar.slice(1)
+        tmp.greek = key.greek + decomposedChar.slice(1)
 
         // If a pair has been found, increase the index (because
         // two letters have been processed) and stop the search.
-        if (mapping[j].trans === pair) i++; break
+        if (key.trans === pair) i++; break
       }
     }
 
-    greekStr += tmp.greek.normalize('NFC') || str[i]
+    greekStr += tmp.greek.normalize('NFC') || transliteratedStr[i]
   }
 
   return greekStr
+}
+
+function recomposeChar (decomposedChar: string): string {
+  let recomposedChar = decomposedChar.charAt(0)
+
+  // \u0302 = circumflex.
+  if (decomposedChar.includes('\u0302')) {
+    recomposedChar += '\u0302'
+  }
+
+  return recomposedChar.normalize('NFC')
 }
