9ba33209c2159b39b15a46102fde36e956f477d3
antoineboquet <antoineboquet@outlook.com>
2021-05-15 12:57:41 +0200

    Update tests

--- tests/toBetaCode.test.ts
+++ tests/toBetaCode.test.ts
@@ -1,14 +1,32 @@
 import { keyType, toBetaCode } from '../src/index'
 
 describe('toBetaCode', () => {
+  const aristotle = { // challenge: `(meta\\ kinh/sews ga/r)`
+    greek: 'Ἐκεῖναι μὲν δὴ φυσικῆς μετὰ κινήσεως γάρ, αὕτη δὲ ἑτέρας, εἰ μηδεμία αὐτοῖς ἀρχὴ κοινή.',
+    bcodeAccented: 'E)kei=nai me\\n dh\\ fusikh=s meta\\ kinh/sews ga/r, au(/th de\\ e(te/ras, ei) mhdemi/a au)toi=s a)rxh\\ koinh/.',
+    bcodeUnaccented: 'Ekeinai men dh fusikhs meta kinhsews gar, auth de eteras, ei mhdemia autois arxh koinh.'
+  }
+
   test.each`
-    str           | expected
-    ${'ανθρωπος'} | ${'anqrwpos'}
-    ${'α α'}      | ${'a a'}
-    ${'ἄνθρωπος'} | ${'anqrwpos'}
-    ${'ἵππος'}    | ${'ippos'}
+    str                | expected
+    ${'ανθρωπος'}      | ${'anqrwpos'}
+    ${'α α'}           | ${'a a'}
+    ${'ἄνθρωπος'}      | ${'anqrwpos'}
+    ${'ἵππος'}         | ${'ippos'}
+    ${aristotle.greek} | ${aristotle.bcodeUnaccented}
   `('Testing `toBetaCode` function w/ greek input, omitting diactrics', ({ str, expected }) => {
     expect(toBetaCode(str, keyType.GREEK, { removeDiacritics: true })).toBe(expected)
+  })
+
+  test.each`
+    str                | expected
+    ${'ἄνθρωπος'}      | ${'a)/nqrwpos'}
+    ${'ἵππος'}         | ${'i(/ppos'}
+    ${'ποιῇ'}          | ${'poih=|'}
+    ${'Ἄϊδα'}          | ${'A)/i+da'}
+    ${aristotle.greek} | ${aristotle.bcodeAccented}
+  `('Testing `toBetaCode` function w/ greek input, omitting diactrics', ({ str, expected }) => {
+    expect(toBetaCode(str, keyType.GREEK)).toBe(expected)
   })
 
   test.each`
