0221549a7ef6d35ccf11da6fa8b7eaad73dfd0e9
antoineboquet <antoineboquet@outlook.com>
2021-05-16 20:56:00 +0200

    Implements fromTransliterationToBetaCode() with diacritics

--- src/toBetaCode.ts
+++ src/toBetaCode.ts
@@ -7,20 +7,38 @@ export function toBetaCode (
   from: keyType,
   options: ConversionOptions = {}
 ): string {
+  if (options.removeDiacritics) str = removeDiacritics(str)
+
   switch (from) {
     case keyType.GREEK:
-      if (options.removeDiacritics) str = removeDiacritics(str)
       str = removeGreekVariants(str)
       str = fromGreekToBetaCode(str, options.removeDiacritics)
       break
 
     case keyType.TRANSLITERATION:
-      if (options.removeDiacritics) str = removeDiacritics(str)
       str = fromTransliterationToBetaCode(str)
       break
   }
 
   return str
+}
+
+function convertTransliteratedDiacritics (decomposedDiacritics: string): string {
+  let betaCodeDiacritics = ''
+
+  for (const point of decomposedDiacritics) {
+    for (const diacritic of diacriticsMapping) {
+      if (point === diacritic.trans) {
+        betaCodeDiacritics += diacritic.latin
+        break
+      }
+    }
+  }
+
+  // Ensure the order (grave/accute/tilde then diaeresis & iota subscript).
+  betaCodeDiacritics = betaCodeDiacritics.replace(/([+|])([\/\\=])/g, '$2$1')
+
+  return betaCodeDiacritics
 }
 
 function fromGreekToBetaCode (
@@ -33,7 +51,9 @@ function fromGreekToBetaCode (
     ? [...greekMapping, ...diacriticsMapping]
     : greekMapping
 
-  if (!removeDiacritics) greekStr = greekStr.normalize('NFD')
+  if (!removeDiacritics) {
+    greekStr = greekStr.normalize('NFD')
+  }
 
   for (const char of greekStr) {
     let tmp: string
@@ -51,17 +71,23 @@ function fromGreekToBetaCode (
   return betaCodeStr
 }
 
-function fromTransliterationToBetaCode (str: string): string {
+function fromTransliterationToBetaCode (transliteratedStr: string): string {
   let betaCodeStr = ''
 
-  for (let i = 0; i < str.length; i++) {
-    const tmp = { trans: undefined, latin: undefined }
+  for (let i = 0; i < transliteratedStr.length; i++) {
+    const tmp = { trans: '', latin: '' }
 
-    let  pair = str.slice(i, i + 2)
-    if (pair.length !== 2) pair = undefined
+    let pair = transliteratedStr.slice(i, (i + 2))
+    if (pair.length !== 2) pair = ''
+
+    let decomposedChar = transliteratedStr[i].normalize('NFD')
+
+    // Isolate the character with its potential circumflex (as this diacritical
+    // mark is used to represent long vowels in transliterated form).
+    let recomposedChar = recomposeChar(decomposedChar)
 
     for (const key of greekMapping) {
-      if ([str[i], pair].includes(key.trans)) {
+      if ([recomposedChar, pair].includes(key.trans)) {
         tmp.trans = key.trans
         tmp.latin = key.latin
 
@@ -69,8 +95,28 @@ function fromTransliterationToBetaCode (str: string): string {
       }
     }
 
-    betaCodeStr += tmp.latin || ((!/h/i.test(str[i])) ? str[i] : '')
+    // Remove base character + the eventual recomposed circumflex (u0302).
+    const decomposedDiacritics = decomposedChar.slice(1).replace(/\u0302/g, '')
+
+    tmp.latin += convertTransliteratedDiacritics(decomposedDiacritics)
+
+    betaCodeStr += tmp.latin || (
+      !/h/i.test(transliteratedStr[i]) // rough breathings aren't converted yet.
+        ? transliteratedStr[i]
+        : ''
+      )
   }
 
   return betaCodeStr
+}
+
+function recomposeChar (decomposedChar: string): string {
+  let recomposedChar = decomposedChar.charAt(0)
+
+  // \u0302 = circumflex (`Combining Circumflex Accent`).
+  if (decomposedChar.includes('\u0302')) {
+    recomposedChar += '\u0302'
+  }
+
+  return recomposedChar.normalize('NFC')
 }
