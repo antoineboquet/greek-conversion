4a4424b17b3e2d6344a60223747a97e8f92e0dec
antoineboquet <antoineboquet@outlook.com>
2021-05-18 22:27:08 +0200

    Update tests

--- tests/toBetaCode.test.ts
+++ tests/toBetaCode.test.ts
@@ -3,6 +3,7 @@ import { keyType, toBetaCode } from '../src/index'
 describe('toBetaCode', () => {
   const aristotle = { // challenge: `(meta\\ kinh/sews ga/r)`
     greek: 'Ἐκεῖναι μὲν δὴ φυσικῆς μετὰ κινήσεως γάρ, αὕτη δὲ ἑτέρας, εἰ μηδεμία αὐτοῖς ἀρχὴ κοινή.',
+    trans: 'Ekeĩnai mèn dề phusikễs metà kinếseôs gár, haútê dè hetéras, ei mêdemía autoĩs archề koinế.',
     bcodeAccented: 'E)kei=nai me\\n dh\\ fusikh=s meta\\ kinh/sews ga/r, au(/th de\\ e(te/ras, ei) mhdemi/a au)toi=s a)rxh\\ koinh/.',
     bcodeUnaccented: 'Ekeinai men dh fusikhs meta kinhsews gar, auth de eteras, ei mhdemia autois arxh koinh.'
   }
@@ -30,20 +31,26 @@ describe('toBetaCode', () => {
   })
 
   test.each`
-    str            | expected
-    ${'anthrôpos'} | ${'anqrwpos'}
-    ${'hippos'}    | ${'ippos'}
-    ${'aḯdalos'}   | ${'aidalos'}
+    str                | expected
+    ${'anthrôpos'}     | ${'anqrwpos'}
+    ${'hippos'}        | ${'ippos'}
+    ${'aḯdalos'}       | ${'aidalos'}
+    ${'Húsiris'}       | ${'Usiris'}
+    ${aristotle.trans} | ${aristotle.bcodeUnaccented}
   `('Testing `toBetaCode` function w/ transliterated input, omitting diactrics', ({ str, expected }) => {
     expect(toBetaCode(str, keyType.TRANSLITERATION, { removeDiacritics: true })).toBe(expected)
   })
 
   test.each`
-    str             | expected
-    ${'ánthrôpos'}  | ${'a/nqrwpos'}
-    ${'prosễlthon'} | ${'prosh=lqon'}
-    ${'aḯdalos'}    | ${'ai/+dalos'}
-    ${'Áïda'}       | ${'A/i+da'}
+    str                | expected
+    ${'ánthrôpos'}     | ${'a)/nqrwpos'}
+    ${'rhuthmós'}      | ${'r(uqmo/s'}
+    ${'prosễlthon'}    | ${'prosh=lqon'}
+    ${'aḯdalos'}       | ${'ai)/+dalos'}
+    ${'Áïda'}          | ${'A)/i+da'}
+    ${'hoplítês'}      | ${'o(pli/ths'}
+    ${'Húsiris'}       | ${'U(/siris'}
+    ${aristotle.trans} | ${aristotle.bcodeAccented}
   `('Testing `toBetaCode` function w/ transliterated input, preserving diactrics', ({ str, expected }) => {
     expect(toBetaCode(str, keyType.TRANSLITERATION)).toBe(expected)
   })
