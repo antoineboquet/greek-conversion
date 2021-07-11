1cbf42b12e3040c8be51bb4de8344da740d07b47
antoineboquet <antoineboquet@outlook.com>
2021-07-11 19:19:06 +0200

    Update test

--- tests/GreekString.test.ts
+++ tests/GreekString.test.ts
@@ -3,6 +3,7 @@ import { keyType, GreekString } from '../src/index'
 describe('GreekString', () => {
   const foo = new GreekString('anthrôpos', keyType.TRANSLITERATION)
   const bar = new GreekString('ἵππος', keyType.GREEK, { removeDiacritics: true })
+  const baz = new GreekString('αἴξ   κριός', keyType.GREEK)
 
   test('Testing `GreekString` object', () => {
     expect(foo.source).toBe('anthrôpos')
@@ -11,10 +12,17 @@ describe('GreekString', () => {
     expect(foo.transliteration).toBe('anthrôpos')
   })
 
-  test('Testing `GreekString` object w/ conversion options', () => {
+  test('Testing `GreekString` object w/ conversion options (removeDiacritics)', () => {
     expect(bar.source).toBe('ἵππος')
     expect(bar.betaCode).toBe('ippos')
-    expect(bar.greek).toBe('ἵππος') // later, we'll want `ιππος`
+    expect(bar.greek).toBe('ιππος')
     expect(bar.transliteration).toBe('ippos')
+  })
+
+  test('Testing `GreekString` object w/ extra whitespace', () => {
+    expect(baz.source).toBe('αἴξ   κριός')
+    expect(baz.betaCode).toBe('ai)/c krio/s')
+    expect(baz.greek).toBe('αἴξ κριός')
+    expect(baz.transliteration).toBe('aíx kriós')
   })
 })
