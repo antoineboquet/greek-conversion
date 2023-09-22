e6ee1398cb58a5068b6db9772c2074be3c2d0e7e
antoineboquet <antoineboquet@users.noreply.github.com>
2023-09-22 19:06:24 +0200

    Update tests

--- tests/GreekString.test.ts
+++ tests/GreekString.test.ts
@@ -1,9 +1,13 @@
 import { keyType, GreekString } from '../src/index'
 
 describe('GreekString', () => {
-  const foo = new GreekString('ánthrôpos', keyType.TRANSLITERATION)
-  const bar = new GreekString('ἵππος', keyType.GREEK, { removeDiacritics: true })
-  const baz = new GreekString('αἴξ   κριός', keyType.GREEK)
+  const foo    = new GreekString('ánthrôpos', keyType.TRANSLITERATION)
+  const bar    = new GreekString('ἵππος', keyType.GREEK, { removeDiacritics: true })
+  const baz    = new GreekString('αἴξ   κριός', keyType.GREEK)
+  const qux    = new GreekString('βάρβαρος', keyType.GREEK)
+  const quux   = new GreekString('βάρβαρος', keyType.GREEK, { disableBetaVariant: true })
+  const corge  = new GreekString('ανγελος', keyType.GREEK)
+  const grault = new GreekString('aggelos', keyType.TRANSLITERATION)
 
   test('Testing `GreekString` object', () => {
     expect(foo.source).toBe('ánthrôpos')
@@ -24,5 +28,25 @@ describe('GreekString', () => {
     expect(baz.betaCode).toBe('ai)/c krio/s')
     expect(baz.greek).toBe('αἴξ κριός')
     expect(baz.transliteration).toBe('aíx kriós')
+  })
+
+  test('Testing `GreekString` object w/ beta variant', () => {
+    expect(qux.source).toBe('βάρβαρος')
+    expect(qux.greek).toBe('βάρϐαρος')
+  })
+
+  test('Testing `GreekString` object w/ conversion options (disableBetaVariant)', () => {
+    expect(quux.source).toBe('βάρβαρος')
+    expect(quux.greek).toBe('βάρβαρος')
+  })
+
+  test('Testing `GreekString` object w/ gamma diphthong (from greek)', () => {
+    expect(corge.source).toBe('ανγελος')
+    expect(corge.greek).toBe('αγγελος')
+  })
+
+  test('Testing `GreekString` object w/ gamma diphthong (from transliteration)', () => {
+    expect(grault.source).toBe('aggelos')
+    expect(grault.transliteration).toBe('angelos')
   })
 })
