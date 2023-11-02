ca66842b40f1cac514c51e8c58bfc89c1618ffdb
antoineboquet <antoineboquet@users.noreply.github.com>
2023-11-02 19:00:01 +0100

    Make private members, add new `Mapping` & update imports

--- src/GreekString.ts
+++ src/GreekString.ts
@@ -1,83 +1,119 @@
-import { keyType } from './enums'
-import { ConversionOptions } from './interfaces'
-import { toBetaCode } from './toBetaCode'
-import { toGreek } from './toGreek'
-import { toTransliteration } from './toTransliteration'
+import { keyType } from './enums';
+import { IConversionOptions } from './interfaces';
+import { Mapping } from './Mapping';
+import { toBetaCode } from './toBetaCode';
+import { toGreek } from './toGreek';
+import { toTransliteration } from './toTransliteration';
 
 export class GreekString {
-  readonly from: keyType
-  readonly options: ConversionOptions
-  readonly source: string
+  readonly from: keyType;
+  readonly options: IConversionOptions;
+  readonly source: string;
 
-  _beta_code: string
-  _greek: string
-  _transliteration: string
+  #betaCode: string;
+  #greek: string;
+  #transliteration: string;
 
-  constructor(str: string, from: keyType, options?: ConversionOptions) {
-    this.from = from
-    this.options = options ?? {}
-    this.source = str
+  constructor(str: string, from: keyType, options?: IConversionOptions) {
+    this.from = from;
+    this.options = options ?? {};
+    this.source = str;
 
     switch (from) {
       case keyType.BETA_CODE:
-        this._beta_code = toBetaCode(str, keyType.BETA_CODE, this.options)
-        break
+        this.#betaCode = toBetaCode(
+          str,
+          keyType.BETA_CODE,
+          this.options,
+          new Mapping(options)
+        );
+        break;
 
       case keyType.GREEK:
-        this._greek = toGreek(str, keyType.GREEK, this.options)
-        break
+        this.#greek = toGreek(
+          str,
+          keyType.GREEK,
+          this.options,
+          new Mapping(options)
+        );
+        break;
 
       case keyType.TRANSLITERATION:
-        this._transliteration = toTransliteration(str, keyType.TRANSLITERATION, this.options)
-        break
+        this.#transliteration = toTransliteration(
+          str,
+          keyType.TRANSLITERATION,
+          this.options,
+          new Mapping(options)
+        );
+        break;
     }
   }
 
   get betaCode(): string {
-    if (!this._beta_code) {
+    if (!this.#betaCode) {
       switch (this.from) {
         case keyType.GREEK:
-          this._beta_code = toBetaCode(this._greek, keyType.GREEK, this.options)
-          break
+          this.#betaCode = toBetaCode(this.#greek, keyType.GREEK, this.options);
+          break;
 
         case keyType.TRANSLITERATION:
-          this._beta_code = toBetaCode(this._transliteration, keyType.TRANSLITERATION, this.options)
-          break
+          this.#betaCode = toBetaCode(
+            this.#transliteration,
+            keyType.TRANSLITERATION,
+            this.options
+          );
+          break;
       }
     }
 
-    return this._beta_code
+    return this.#betaCode;
   }
 
   get greek(): string {
-    if (!this._greek) {
+    if (!this.#greek) {
       switch (this.from) {
         case keyType.BETA_CODE:
-          this._greek = toGreek(this._beta_code, keyType.BETA_CODE, this.options)
-          break
+          this.#greek = toGreek(
+            this.#betaCode,
+            keyType.BETA_CODE,
+            this.options
+          );
+          break;
 
         case keyType.TRANSLITERATION:
-          this._greek = toGreek(this._transliteration, keyType.TRANSLITERATION, this.options)
-          break
+          this.#greek = toGreek(
+            this.#transliteration,
+            keyType.TRANSLITERATION,
+            this.options
+          );
+          break;
       }
     }
 
-    return this._greek
+    return this.#greek;
   }
 
   get transliteration(): string {
-    if (!this._transliteration) {
+    if (!this.#transliteration) {
       switch (this.from) {
         case keyType.BETA_CODE:
-          this._transliteration = toTransliteration(this._beta_code, keyType.BETA_CODE, this.options)
-          break
+          this.#transliteration = toTransliteration(
+            this.#betaCode,
+            keyType.BETA_CODE,
+            this.options
+          );
+          break;
 
         case keyType.GREEK:
-          this._transliteration = toTransliteration(this._greek, keyType.GREEK, this.options)
-          break
+          this.#transliteration = toTransliteration(
+            this.#greek,
+            keyType.GREEK,
+            this.options
+          );
+          break;
       }
     }
 
-    return this._transliteration
+    return this.#transliteration;
   }
 }
