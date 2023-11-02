e8f6e08ac788c5b3478459aab3af456fbb666542
antoineboquet <antoineboquet@users.noreply.github.com>
2023-11-02 19:00:57 +0100

    Export `additionalLetters` enum

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { keyType } from './enums'
-import { GreekString } from './GreekString'
-import { toBetaCode } from './toBetaCode'
-import { toGreek } from './toGreek'
-import { toTransliteration } from './toTransliteration'
+import { additionalLetters, keyType } from './enums';
+import { GreekString } from './GreekString';
+import { toBetaCode } from './toBetaCode';
+import { toGreek } from './toGreek';
+import { toTransliteration } from './toTransliteration';
 import {
   applyGammaDiphthongs,
   applyGreekVariants,
   isMappedKey,
   removeDiacritics,
   removeGreekVariants
-} from './utils'
+} from './utils';
 
-export { keyType } from './enums'
-export { GreekString } from './GreekString'
-export { toBetaCode } from './toBetaCode'
-export { toGreek } from './toGreek'
-export { toTransliteration } from './toTransliteration'
+export { additionalLetters, keyType } from './enums';
+export { GreekString } from './GreekString';
+export { toBetaCode } from './toBetaCode';
+export { toGreek } from './toGreek';
+export { toTransliteration } from './toTransliteration';
 export {
   applyGammaDiphthongs,
   applyGreekVariants,
   isMappedKey,
   removeDiacritics,
   removeGreekVariants
-} from './utils'
+} from './utils';
 
 export default {
   keyType,
@@ -35,4 +35,4 @@ export default {
   isMappedKey,
   removeDiacritics,
   removeGreekVariants
-}
+};
