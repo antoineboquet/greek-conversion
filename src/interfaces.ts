e5e72fc1ca1fd793739143481da4407b6d2895fe
antoineboquet <antoineboquet@users.noreply.github.com>
2023-11-02 17:50:43 +0100

    Add new conversion options

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,21 @@
-export interface ConversionOptions {
-  preserveWhitespace?: boolean,
-  removeDiacritics?: boolean,
-  disableBetaVariant?: boolean
+import { additionalLetters } from './enums';
+
+export interface IConversionOptions {
+  preserveWhitespace?: boolean;
+  removeDiacritics?: boolean;
+  useAdditionalLetters?: additionalLetters | additionalLetters[];
+  setGreekStyle?: IGreekStyle;
+  setTransliterationStyle?: ITransliterationStyle;
+}
+
+export interface IGreekStyle {
+  disableBetaVariant?: boolean;
+  useSemicolonAsQuestionMark?: boolean;
+}
+
+export interface ITransliterationStyle {
+  useCxOverMacron?: boolean;
+  xi_ks?: boolean;
+  chi_kh?: boolean;
+  //upsilon_y?: boolean;
 }
