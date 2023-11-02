6935b4cf5590ee82c6e75346772e7bcd9621bc49
antoineboquet <antoineboquet@users.noreply.github.com>
2023-11-02 17:50:09 +0100

    Add `additionalLetters`

--- src/enums.ts
+++ src/enums.ts
@@ -3,3 +3,14 @@ export enum keyType {
   BETA_CODE = 'beta-code',
   TRANSLITERATION = 'transliteration'
 }
+
+export enum additionalLetters {
+  ALL = 'all',
+  DIGAMMA = 'digamma',
+  YOT = 'yot',
+  LUNATE_SIGMA = 'lunate-sigma',
+  STIGMA = 'stigma',
+  KOPPA = 'koppa',
+  SAMPI = 'sampi',
+  //SAN = 'san'
+}
