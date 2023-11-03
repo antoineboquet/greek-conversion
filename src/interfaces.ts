8f3766ceac0c863428d8a2ccda1b777963f2baef
antoineboquet <antoineboquet@users.noreply.github.com>
2023-11-03 16:53:25 +0100

    Add `IBetaCodeStyle` & `IConversionOptions.setBetaCodeStyle`

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,8 +4,13 @@ export interface IConversionOptions {
   preserveWhitespace?: boolean;
   removeDiacritics?: boolean;
   useAdditionalLetters?: additionalLetters | additionalLetters[];
+  setBetaCodeStyle?: IBetaCodeStyle;
   setGreekStyle?: IGreekStyle;
   setTransliterationStyle?: ITransliterationStyle;
+}
+
+export interface IBetaCodeStyle {
+  classical?: boolean;
 }
 
 export interface IGreekStyle {
