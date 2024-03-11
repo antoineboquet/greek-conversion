b413b9903292faefc5c9186a685424a664e43be4
antoineboquet <antoineboquet@users.noreply.github.com>
2024-03-11 10:21:24 +0100

    Add esbuild config

---
+++ esbuild.config.js
@@ -0,0 +1,22 @@
+import * as esbuild from 'esbuild';
+
+const sharedSettings = {
+  logLevel: 'info',
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  sourcemap: true,
+  minify: true,
+  mangleProps: /^CAPITAL|^SMALL|^PUNCT|^DIA/
+};
+
+await esbuild.build({
+  ...sharedSettings,
+  format: 'esm',
+  outfile: './dist/esbuild.min.js'
+});
+
+await esbuild.build({
+  ...sharedSettings,
+  format: 'cjs',
+  outfile: './dist/esbuild.min.cjs'
+});
