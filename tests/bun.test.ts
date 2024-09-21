512a7ec5d8ad4a9adf10d583cccbbd4d420faa38
antoineboquet <antoineboquet@users.noreply.github.com>
2024-09-21 19:11:36 +0200

    Update tests

--- tests/bun.test.ts
+++ tests/bun.test.ts
@@ -2,49 +2,61 @@ import { expect, test } from 'bun:test';
 import { toGreek } from '../src/toGreek';
 import { KeyType } from '../src/enums';
 
-const basicConversion = [
-  {
-    str: 'a)/nqrwpos',
-    expected: 'ἄνθρωπος'
-  },
-  {
-    str: 'kalo\\s ka)gaqo/s',
-    expected: 'καλὸς κἀγαθός'
-  },
-  {
-    str: 'au)to/nomos',
-    expected: 'αὐτόνομος'
-  },
-  {
-    str: 'poih|=',
-    expected: 'ποιῇ'
-  },
-  {
-    str: 'A)/i+da',
-    expected: 'Ἄϊδα'
-  },
-  {
-    str: 'ba/rbaros',
-    expected: 'βάρβαρος'
-  },
-  {
-    str: 'O(pli/ths',
-    expected: 'Ὁπλίτης'
-  },
-  {
-    str: 'voi=',
-    expected: 'vοῖ'
-  },
-  {
-    str: 'a(/gios3',
-    expected: 'ἅγιοσ3'
-  },
-  {
-    str: 'a)%27a%26ehi%27i%26owu%27u%26',
-    expected: 'ἀ̆ᾱεηῐῑοωῠῡ'
-  }
-];
+const tests = {
+  'basic-conversion': [
+    {
+      i: 'ABGDEZHQIKLMNCOPRSTYFXYW',
+      o: 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΥΩ'
+    },
+    {
+      i: 'abgdezhqiklmncoprstyfxyw',
+      o: 'αβγδεζηθικλμνξοπρστυφχψω'
+    }
+    /*{
+      str: 'a)/nqrwpos',
+      expected: 'ἄνθρωπος'
+    },
+    {
+      str: 'kalo\\s ka)gaqo/s',
+      expected: 'καλὸς κἀγαθός'
+    },
+    {
+      str: 'au)to/nomos',
+      expected: 'αὐτόνομος'
+    },
+    {
+      str: 'poih|=',
+      expected: 'ποιῇ'
+    },
+    {
+      str: 'A)/i+da',
+      expected: 'Ἄϊδα'
+    },
+    {
+      str: 'ba/rbaros',
+      expected: 'βάρβαρος'
+    },
+    {
+      str: 'O(pli/ths',
+      expected: 'Ὁπλίτης'
+    },
+    {
+      str: 'voi=',
+      expected: 'vοῖ'
+    },
+    {
+      str: 'a(/gios3',
+      expected: 'ἅγιοσ3'
+    },
+    {
+      str: 'a)%27a%26ehi%27i%26owu%27u%26',
+      expected: 'ἀ̆ᾱεηῐῑοωῠῡ'
+    }*/
+  ]
+};
 
-test.each(basicConversion)('Basic conversion', ({ str, expected }) => {
-  expect(toGreek(str, KeyType.SIMPLE_BETA_CODE)).toBe(expected);
-});
+for (const key of Object.keys(tests)) {
+  test.each(tests[key])(key.replaceAll('-', ' '), ({ i, o }) => {
+    expect(toGreek(i, KeyType.SIMPLE_BETA_CODE)).toBe(o);
+  });
+}
