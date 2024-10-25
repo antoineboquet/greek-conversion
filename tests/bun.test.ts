2de2a9906988545a610b2800de62c8c93ad24b2a
antoineboquet <antoineboquet@users.noreply.github.com>
2024-10-25 14:50:20 +0200

    Update tests

--- tests/bun.test.ts
+++ tests/bun.test.ts
@@ -1,16 +1,20 @@
-import { expect, test } from 'bun:test';
+//import { expect, test } from 'bun:test';
 import { toGreek } from '../src/toGreek';
 import { KeyType } from '../src/enums';
 
-const tests = {
+const simpleBetaCodeToGreek = {
   'basic-conversion': [
     {
-      i: 'ABGDEZHQIKLMNCOPRSTYFXYW',
-      o: 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΥΩ'
+      i: 'ABGDEZHQIKLMNCOPRSTUFXYW',
+      o: 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ'
     },
     {
-      i: 'abgdezhqiklmncoprstyfxyw',
+      i: 'abgdezhqiklmncoprstufxyw',
       o: 'αβγδεζηθικλμνξοπρστυφχψω'
+    },
+    {
+      i: 'a)/a(/a%27a%26h|=i+',
+      o: 'ἄἅᾰᾱῇϊ'
     },
     {
       i: 'a)/nqrwpos',
@@ -39,24 +43,47 @@ const tests = {
     {
       i: 'O(pli/ths',
       o: 'Ὁπλίτης'
-    },
-    {
+    }
+    /*{
       i: 'voi=',
       o: 'vοῖ'
     },
     {
       i: 'a(/gios3',
       o: 'ἅγιοσ3'
-    }
-    /*{
+    },
+    {
       str: 'a)%27a%26ehi%27i%26owu%27u%26',
       expected: 'ἀ̆ᾱεηῐῑοωῠῡ'
     }*/
   ]
 };
 
-for (const key of Object.keys(tests)) {
-  test.each(tests[key])(key.replaceAll('-', ' '), ({ i, o }) => {
+for (const key of Object.keys(simpleBetaCodeToGreek)) {
+  test.each(simpleBetaCodeToGreek[key])(key, ({ i, o }) => {
     expect(toGreek(i, KeyType.SIMPLE_BETA_CODE)).toBe(o);
+  });
+}
+
+const transliterationToGreek = {
+  'basic-conversion': [
+    {
+      i: 'ABGDEZĒThIKLMNXOPRSTUPhChPsŌ',
+      o: 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ'
+    },
+    {
+      i: 'abgdezēthiklmnxoprstuphchpsō',
+      o: 'αβγδεζηθικλμνξοπρστυφχψω'
+    },
+    {
+      i: 'ē',
+      o: 'η'
+    }
+  ]
+};
+
+for (const key of Object.keys(transliterationToGreek)) {
+  test.each(transliterationToGreek[key])(key, ({ i, o }) => {
+    expect(toGreek(i, KeyType.TRANSLITERATION)).toBe(o);
   });
 }
