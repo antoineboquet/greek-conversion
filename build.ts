8f71df3245dae313a7e0cf40f09e109ce9c3d74f
antoineboquet <antoineboquet@users.noreply.github.com>
2024-10-25 15:07:17 +0200

    Add `cjs` build

--- build.ts
+++ build.ts
@@ -1,26 +1,29 @@
 import { BuildConfig } from 'bun';
+import fs from 'node:fs/promises';
 
 const name = 'greekConversion.min';
 const outdir = './dist';
+
+await fs.rm(outdir, { recursive: true, force: true });
 
 // @fixme: variable names mangling is missing (for mapping schemas).
 const commonBuildConfig: BuildConfig = {
   entrypoints: ['./src/index.ts'],
   minify: true,
-  naming: `[dir]/${name}.[ext]`,
   outdir: outdir,
   sourcemap: 'linked',
   target: 'browser'
 };
 
-// @fixme: Cjs format isn't supported yet in Bun 1.1.29.
 const results = await Promise.all([
-  /*Bun.build({
-    ...commonBuildConfig,
-    format: 'cjs'
-  }),*/
   Bun.build({
     ...commonBuildConfig,
+    naming: `[dir]/${name}.cjs`,
+    format: 'cjs'
+  }),
+  Bun.build({
+    ...commonBuildConfig,
+    naming: `[dir]/${name}.mjs`,
     format: 'esm'
   })
 ]);
