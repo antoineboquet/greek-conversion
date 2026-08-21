88afab28957d5a23e9bccf57052b10b57fe869d9
antoineboquet <4170700+antoineboquet@users.noreply.github.com>
2026-08-21 20:54:07 +0200

    Refactoring + Morpheus  enhancements

--- src/model/lookup.ts
+++ src/model/lookup.ts
@@ -2,6 +2,7 @@ import {
   KeyType,
   removeDiacritics,
   removeGreekVariants,
+  toBetaCode,
   toGreek
 } from "greek-conversion";
 import { Database } from "../Database.ts";
@@ -16,9 +17,9 @@ import type {
 } from "../definitions.ts";
 import { SpecialChar } from "../enums.ts";
 import { setUniqueEntries } from "../helpers.ts";
-import { Morpheus } from "../Morpheus.ts";
+import { Morpheus, type MorpheusResponse } from "../Morpheus.ts";
 import { Settings } from "../Settings.ts";
-import { type Morphology } from "../MorpheusParser.ts";
+import type { MorpheusAnalysis, Morphology } from "../MorpheusParser.ts";
 
 type MorphologyKey = {
   lemma: string;
@@ -27,10 +28,7 @@ type MorphologyKey = {
 
 function parseMorphologyKey(key: string): MorphologyKey | undefined {
   const separatorIndex = key.indexOf("|");
-
-  if (separatorIndex === -1) {
-    return undefined;
-  }
+  if (separatorIndex === -1) return undefined;
 
   return {
     lemma: key.slice(0, separatorIndex),
@@ -39,11 +37,12 @@ function parseMorphologyKey(key: string): MorphologyKey | undefined {
 }
 
 function attachMorphology<K extends keyof QueryableFields>(
-  response: ApiLookupResponse<K> | ApiLookupResponse<never>
-): ApiLookupResponse<K> | ApiLookupResponse<never> {
-  const entryWords = new Set(response.data.entries.map(({ word }) => word));
+  partialResponse: ApiLookupResponse<K>,
+  orphanMorphology: MorpheusResponse<Morphology>
+): ApiLookupResponse<K> {
+  const entryWords = new Set(partialResponse.data.entries.map(({ word }) => word));
 
-  const { byLemma, orphans } = Object.entries(response.data.orphanMorphology)
+  const { byLemma, orphans } = Object.entries(orphanMorphology)
     .reduce(
       (acc, [compoundKey, analyses]) => {
         const parsed = parseMorphologyKey(compoundKey);
@@ -61,19 +60,15 @@ function attachMorphology<K extends keyof QueryableFields>(
         return acc;
       },
       {
-        byLemma: {} as Record<
-          string,
-          Record<string, Morphology[]>
-        >,
+        byLemma: {} as Record<string, Record<string, Morphology[]>>,
         orphans: {} as Record<string, Morphology[]>
       }
     );
 
-  return {
+  let response = {
     data: {
-      ...response.data,
-      orphanMorphology: orphans,
-      entries: response.data.entries.map((entry) => ({
+      ...partialResponse.data,
+      entries: partialResponse.data.entries.map((entry) => ({
         ...entry,
         ...(byLemma[entry.word] && {
           morphology: byLemma[entry.word]
@@ -81,6 +76,12 @@ function attachMorphology<K extends keyof QueryableFields>(
       }))
     }
   };
+
+  if (Settings.getSettings().isDevEnv) {
+    response.data.orphanMorphology = orphans;
+  }
+
+  return response;
 }
 
 function emptyResponse(): ApiLookupResponse<never> {
@@ -94,21 +95,62 @@ function emptyResponse(): ApiLookupResponse<never> {
   };
 }
 
+function normalizeGreekForLookup(word: string): string {
+  return word
+    // @fixme NFC/NFKC normalization breaks matches with the data converter.
+    //.normalize("NFD")
+    //.replace(/[\u0304\u0306]/g, "") // macron, breve
+    //.normalize("NFC")
+    .replace(/[·•]/g, "");
+}
+
 function formatQueryStr(
-  str: string,
-  isExactMatch: boolean,
-  isCaseSensitive: boolean
+  q: string,
+  exactMatchQuery: boolean,
+  caseSensitive: boolean,
+  diacriticSensitive: boolean
 ): string {
-  if (!isExactMatch) {
-    if (str.endsWith(SpecialChar.explicitEnd)) {
-      if (str.startsWith(SpecialChar.wildcard)) str = str.slice(0, -1);
-      else str = SpecialChar.wildcard + str.slice(0, -1);
+  if (exactMatchQuery) {
+    if (
+      (q.startsWith("^") && q.endsWith("$")) ||
+      (q.startsWith('"') && q.endsWith('"'))
+    ) {
+      // Remove the wrapping characters from the query (`isExactMatchQuery` supra will
+      // determine the comparison operator in the SQL statement below).
+      q = q.slice(1, -1);
+    } else if (q.startsWith(SpecialChar.explicitStart) || q.startsWith('"')) {
+      q = q.slice(1);
+    }
+  } else {
+    if (q.endsWith(SpecialChar.explicitEnd)) {
+      if (q.startsWith(SpecialChar.wildcard)) q = q.slice(0, -1);
+      else q = SpecialChar.wildcard + q.slice(0, -1);
     } else {
-      if (!str.endsWith(SpecialChar.wildcard)) str += SpecialChar.wildcard;
+      if (!q.endsWith(SpecialChar.wildcard)) q += SpecialChar.wildcard;
     }
   }
 
-  return isCaseSensitive ? str : str.toLowerCase();
+  // @fixme this is a Morpheus/Bailly concordance rule. It should be extracted from this.
+  const preserveAccents: boolean = (() => {
+    const qAsBetaCode = toBetaCode(q, KeyType.GREEK, {
+      betaCodeStyle: {
+        skipSanitization: true,
+        useTLGStyle: true
+      }
+    }).toLowerCase();
+    return ["ti\\s"].includes(qAsBetaCode);
+  })();
+
+  q = removeGreekVariants(q.trim(), { preserveAccents });
+
+  q = caseSensitive ? q : q.toLowerCase();
+  if (!diacriticSensitive) q = removeDiacritics(q, KeyType.GREEK);
+
+  // Remove the eventual extended contract verb form (e.g. '-ῶ'), as the
+  // `searchable*` database columns don't use it, contrary to the `word` column.
+  q = diacriticSensitive ? q.replace(/(?<=ω)-ῶ$/, "") : q.replace(/(?<=ω)-[ῶω]$/, "");
+
+  return normalizeGreekForLookup(q);
 }
 
 function formatSearchableField(
@@ -120,18 +162,36 @@ function formatSearchableField(
   }`;
 }
 
-/**
- * Returns a string without the extended contract verb form (e.g. '-ῶ'), as the
- * `searchable*` database columns don't use it, contrary to the `word` column.
- * @param greekStr
- */
-function normalizeSearchableKey(
+async function getMorpheusAnalyses(
   greekStr: string,
+  caseSensitive: boolean,
   diacriticSensitive: boolean
-): string {
-  return diacriticSensitive
-    ? greekStr.replace(/(?<=ω)-ῶ$/, "")
-    : greekStr.replace(/(?<=ω)-[ῶω]$/, "");
+): Promise<MorpheusResponse<MorpheusAnalysis>> {
+  const morpheus = await Morpheus.getMorpheus();
+
+  // Remove the eventual GLOB special chars from the Morpheus search.
+  return await morpheus.lookup(greekStr.replace(/^[*?]|[*?]$/, ""), {
+    caseSensitive,
+    diacriticSensitive
+  });
+}
+
+function getMorpheusAnalysesSearchableKeys(
+  analyses: MorpheusResponse<MorpheusAnalysis>
+): string[] {
+  // We want the lemma to compare it against one of the database `searchable*` column later.
+  return Object.keys(analyses).map((key) => {
+    const lemma = parseMorphologyKey(key).lemma;
+
+    const formatted = formatQueryStr(
+      lemma,
+      true, // Morpheus keys are exact match queries.
+      true, // Morpheus keys are matched against the `searchable` (case-sensitive) field.
+      true // Morpheus keys are matched against the `searchable` (diacritic-sensitive) field.
+    );
+
+    return formatted;
+  });
 }
 
 /**
@@ -144,15 +204,18 @@ function normalizeSearchableKey(
  *    (4) Only allow `$` in last position;
  *    (5) Allow a maximum of three identical characters in a row.
  */
-function validateInput(str: string): boolean {
-  if (!str) return false;
-  if (str.length === 1) return /[^α-ωϝ]/i.test(str) === false;
+function validateQueryStr(greekStr: string): boolean {
+  // Validate the user input against a non-accented string.
+  greekStr = removeDiacritics(greekStr, KeyType.GREEK);
+
+  if (!greekStr) return false;
+  if (greekStr.length === 1) return /[^α-ωϝ]/i.test(greekStr) === false;
   return (
-    str.length < 50 &&
-    /[^α-ωϝ\s^$?*"]/i.test(str) === false &&
-    /^.+\^/.test(str) === false &&
-    /\$.+$/.test(str) === false &&
-    /(.)\1{3,}/.test(str) === false
+    greekStr.length < 50 &&
+    /[^α-ωϝ\s^$?*"]/i.test(greekStr) === false &&
+    /^.+\^/.test(greekStr) === false &&
+    /\$.+$/.test(greekStr) === false &&
+    /(.)\1{3,}/.test(greekStr) === false
   );
 }
 
@@ -169,83 +232,75 @@ export async function getEntries<K extends keyof QueryableFields>({
   ApiLookupResponse<K> | ApiLookupResponse<never>
 > {
   const db = await Database.getConnection();
-  const morpheus = await Morpheus.getMorpheus();
   const settings = Settings.getSettings();
 
+  // A query only targets exact matches if it's wrapped within ^ and $ or quotes.
+  const isExactMatchQuery: boolean = (q.startsWith("^") && q.endsWith("$")) ||
+    (q.startsWith('"') && q.endsWith('"'));
+
+  // Convert non-greek inputs to greek.
   if ([KeyType.BETA_CODE, KeyType.TRANSLITERATION].includes(inputMode)) {
     q = toGreek(q, inputMode);
   }
 
-  const fieldsAsStr: string = fields.join(", ");
-
-  const isExactMatch: boolean = (q.startsWith("^") && q.endsWith("$")) ||
-    (q.startsWith('"') && q.endsWith('"'));
-
-  if (isExactMatch) {
-    q = q.slice(1, -1);
-  } else if (q.startsWith(SpecialChar.explicitStart) || q.startsWith('"')) {
-    q = q.slice(1);
-  }
-
-  if (!diacriticSensitive) q = removeDiacritics(q, KeyType.GREEK);
-
-  const searchStr: string = normalizeSearchableKey(
-    removeGreekVariants(q.trim()),
+  const searchStr: string = formatQueryStr(
+    q,
+    isExactMatchQuery,
+    caseSensitive,
     diacriticSensitive
   );
 
-  if (!validateInput(removeDiacritics(searchStr, KeyType.GREEK))) {
+  if (!validateQueryStr(searchStr)) {
+    if (settings.isDevEnv) {
+      console.log(
+        `%cInvalid input '${
+          removeDiacritics(searchStr, KeyType.GREEK)
+        }' (will return an empty response).`,
+        "color:orange"
+      );
+    }
     return emptyResponse();
   }
 
-  const comparisonOperator: string = isExactMatch ? "=" : "GLOB";
+  // Field `word` is needed to retrieve unique entries and build the `children` property.
+  if (!fields.includes("word")) fields.unshift("word");
+  const fieldsAsStr: string = fields.join(", ");
 
   const searchableField: string = formatSearchableField(
     caseSensitive,
     diacriticSensitive
   );
 
-  const morpheusAnalyses = !skipMorpheus
-    ? await morpheus.lookup(searchStr, { caseSensitive, diacriticSensitive })
+  const morpheusAnalyses = (!skipMorpheus)
+    ? await getMorpheusAnalyses(searchStr, caseSensitive, diacriticSensitive)
     : {};
 
-  // We want the lemma to compare it against one of the database `searchable*` column later.
-  const morpheusAnalysesSearchableKeys = Object.keys(morpheusAnalyses).map((
-    key
-  ) => normalizeSearchableKey(parseMorphologyKey(key).lemma, diacriticSensitive));
+  const morpheusAnalysesSearchableKeys = getMorpheusAnalysesSearchableKeys(
+    morpheusAnalyses
+  );
 
-  if (settings.isDevEnv) {
-    console.info(
-      "%c./src/model/lookup.ts > getEntries():",
-      "font-weight:bold",
-      "morpheusAnalysesSearchableKeys =",
-      morpheusAnalysesSearchableKeys
-    );
-  }
-
-  const morpheusSQLStatements: string = (() => {
-    if (morpheusAnalysesSearchableKeys.length) {
-      return "OR searchable IN (" + morpheusAnalysesSearchableKeys
-        .map((_, i) => `$lemma${(i += 1)}`)
-        .join(", ") +
-        ")";
-    }
-    return "";
-  })();
+  const morpheusSQLStatements: string = morpheusAnalysesSearchableKeys.length
+    ? "OR searchable IN (" + morpheusAnalysesSearchableKeys
+      .map((_, i) => `$lemma${(i += 1)}`)
+      .join(", ") +
+      ")"
+    : "";
 
   // The `word` field is mandatory in order to retrieve unique entries and build the
   // `children` property.
   const sql = `
-    SELECT ${!fieldsAsStr.includes("word") ? `word, ${fieldsAsStr}` : fieldsAsStr},
+    SELECT ${fieldsAsStr},
            ${searchableField}, COUNT(*) OVER () AS countAll
     FROM bailly
-    WHERE ${searchableField} ${comparisonOperator} $query ${morpheusSQLStatements}
+    WHERE ${searchableField} ${
+    isExactMatchQuery ? "=" : "GLOB"
+  } $query ${morpheusSQLStatements}
     ORDER BY orderedID
     LIMIT $limit 
   `;
 
   const params: { [key: string]: any } = {
-    $query: formatQueryStr(searchStr, isExactMatch, caseSensitive),
+    $query: searchStr,
     $limit: (() => {
       if (limit && limit <= settings.queryMaxRows) return limit;
       // @fixme One shouldn't have to think about this special value.
@@ -255,15 +310,15 @@ export async function getEntries<K extends keyof QueryableFields>({
 
   morpheusAnalysesSearchableKeys.forEach((lemma, i) => {
     const propName: string = `$lemma${(i += 1)}`;
-    params[propName] = removeGreekVariants(lemma);
+    params[propName] = lemma;
   });
 
-  /*if (Deno.env.get("DENO_ENV") === "development") {
-    console.info(`\n${import.meta.url} > getEntries():\n`);
+  /*if (settings.isDevEnv) {
+    console.log(`\n${import.meta.url} > getEntries():\n`);
     console.log({
       searchStr: searchStr,
       morpheusAnalysesSearchableKeys: morpheusAnalysesSearchableKeys,
-      params: params,
+      params: params
     });
     console.log(sql);
   }*/
@@ -272,27 +327,13 @@ export async function getEntries<K extends keyof QueryableFields>({
     db.prepare(sql).all(params)
   );
 
-  if (!data.length) return emptyResponse();
-
   const uniqueEntries = setUniqueEntries(data, { caseSensitive });
 
-  const response = attachMorphology({
+  let response: ApiLookupResponse<keyof QueryableFields> | ApiLookupResponse<never> = {
     data: {
       version: settings.dbVersion,
       count: data.length,
-      countAll: data[0]?.countAll ?? NaN,
-      // This is post-processed by `attachMorphology()` (see supra).
-      orphanMorphology: (() => {
-        if (morphology) {
-          return Object.fromEntries(
-            Object.entries(morpheusAnalyses).map(([lemma, analyses]) => [
-              lemma,
-              analyses.map(({ morphology }) => morphology)
-            ])
-          );
-        }
-        return {} as any;
-      })(),
+      countAll: data[0]?.countAll ?? 0,
       entries: uniqueEntries.map((item) => {
         const searchableFieldValue =
           item[formatSearchableField(caseSensitive, diacriticSensitive)];
@@ -301,15 +342,11 @@ export async function getEntries<K extends keyof QueryableFields>({
         const isExact: boolean = normalizedSearchStr === searchableFieldValue;
 
         const isMorpheus: boolean = (() => {
-          if (!morpheusAnalysesSearchableKeys.length) {
-            return false;
-          } else if (
-            isExactMatch && searchableFieldValue.length !== searchStr.length
-          ) {
+          if (!morpheusAnalysesSearchableKeys.length) return false;
+          if (isExactMatchQuery && searchableFieldValue.length !== searchStr.length) {
             return true;
-          } else {
-            return !searchableFieldValue.startsWith(normalizedSearchStr);
           }
+          return !searchableFieldValue.startsWith(normalizedSearchStr);
         })();
 
         const removeExtraFields = (
@@ -329,7 +366,7 @@ export async function getEntries<K extends keyof QueryableFields>({
           delete item.searchableCaseInsensitive;
           delete item.searchableAtonic;
           delete item.searchableAtonicCaseInsensitive;
-          // Property `word` has been picked in order to group entries.
+          // Property `word` has been picked in order to group entries (see supra).
           if (!fieldsAsStr.includes("word")) delete item.word;
         };
 
@@ -343,10 +380,18 @@ export async function getEntries<K extends keyof QueryableFields>({
         };
       })
     }
-  });
+  };
 
-  if (!settings.isDevEnv) {
-    delete response.data.orphanMorphology;
+  if (morphology) {
+    response = attachMorphology(
+      response,
+      Object.fromEntries(
+        Object.entries(morpheusAnalyses).map(([lemma, analyses]) => [
+          lemma,
+          analyses.map(({ morphology }) => morphology)
+        ])
+      ) as any
+    );
   }
 
   return response;
